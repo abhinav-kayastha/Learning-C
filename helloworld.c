622cddf6cbf5aa2cc404029ae751e04aa749601e
abhin <abhinavk@metropolia.fi>
2023-06-04 17:11:39 +0300

    learning variables, and basic data types

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/* A preprocesser command that tells the compiler to 
+/* A preprocesser command that tells the compiler to
 include contents of the file "standard input output",
 which contains useful functions for input and output
 */
@@ -11,11 +11,10 @@ int main() // Main function
     procedurally
     */
 
-    printf("I like pizza.\n");
-    printf("It's really good.");
+    printf("Hello World!");
     return 0;
 
-    /* Termination of statements returns 0 when 
+    /* Termination of statements returns 0 when
     program runs succesfully
     */
 }

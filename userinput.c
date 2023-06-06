a4ec82ef72991bed4513b65a353729aaec94046d
abhin <abhinavk@metropolia.fi>
2023-06-06 21:48:07 +0300

    learning user input

---
+++ userinput.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[25]; // initializing variables
+    int age;
+
+    printf("Enter your name: ");
+    scanf("%s", &name); // reading user input and
+                        // and relaying input to
+                        // variable name
+
+    printf("Enter your age: ");
+    scanf("%d", &age); // same as above but for var. age
+
+    // showing user info
+
+    printf("Your name is %s", name);
+    printf(", and you are %d years old.", age);
+
+    return 0;
+}

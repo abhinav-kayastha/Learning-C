f108ab86d418366efa3e1dfcd408e146864464ee
Abhinav Kayastha <abhinavk@metropolia.fi>
2023-06-20 19:13:04 +0300

    learning if-else and reordering rep to fit syllabus

---
+++ 1. Basics of C Programming/if-else.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    if (condition) {
+        code runs if condition is true
+    }
+    else {
+        code runs, if above if-statement is false
+    }
+    */
+
+    if (5 < 6) {
+        printf("first num is smaller than second num.\n");
+    }
+    else {
+        printf("first num is larger than second num.");
+    }
+}

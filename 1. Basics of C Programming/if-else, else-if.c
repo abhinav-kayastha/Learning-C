4744ffc6a8a1e2bf4899ae79377ef5452b39c9a3
Abhinav Kayastha <abhinavk@metropolia.fi>
2023-06-20 19:21:01 +0300

    adding else-if

--- 1. Basics of C Programming/if-else, else-if.c
+++ 1. Basics of C Programming/if-else, else-if.c
@@ -3,8 +3,11 @@
 int main()
 {
     /*
-    if (condition) {
+    if (condition 1) {
         code runs if condition is true
+    }
+    else if (condition 2) {
+        code runs if condition 1 false and condition 2 is true
     }
     else {
         code runs, if above if-statement is false
@@ -15,8 +18,12 @@ int main()
     {
         printf("first num is smaller than second num.\n");
     }
+    else if (5 > 6)
+    {
+        printf("first num is greater than second num.\n");
+    }
     else
     {
-        printf("first num is larger than second num.");
+        printf("first num is equal to second num.");
     }
 }

400f086bd2fca430287ffde0ad36a1f906796ccc
Abhinav Kayastha <abhinavk@metropolia.fi>
2023-06-21 16:24:55 +0300

    learned while loop

---
+++ 3. Loops/while_loop.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    syntax:
+
+    while (condition)
+    {
+        code to be exevuted
+    }
+    */
+    int x = 0;
+
+    while (x <= 5)
+    {
+        printf("%d\n", x);
+        x++;
+    }
+}

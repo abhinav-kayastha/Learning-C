15618f85eb5f7b1899608840193eb582f266e68d
Abhinav Kayastha <abhinavk@metropolia.fi>
2023-06-23 13:32:20 +0300

    learning modular programming: making header file and using that

---
+++ 4. Functions and Modular Programming/arithmetic_functions.h
@@ -0,0 +1,24 @@
+#ifndef arithmetic_functions // use to check if same file name has been used, if so, then it skips code block
+#define arithmetic_functions // defining header file name
+
+int addition(int num1, int num2)
+{
+    return num1 + num2;
+}
+
+int subtraction(int num1, int num2)
+{
+    return num1 - num2;
+}
+
+int multiplication(int num1, int num2)
+{
+    return num1 * num2;
+}
+
+float division(int num1, int num2)
+{
+    return num1 / num2;
+}
+
+#endif // ending contents of header file
---
+++ 4. Functions and Modular Programming/use_of_arithmetic_functions_header.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <C:\Users\Abhinav Kayastha\Learning-C\4. Functions and Modular Programming\arithmetic_functions.h>
+
+int main()
+{
+    int sum = addition(1, 2);
+    int difference = subtraction(1, 2);
+    int product = multiplication(1, 2);
+    float quotient = division(1, 2);
+
+    printf("\nSum: %d\n", sum);
+    printf("Difference: %d\n", difference);
+    printf("Product: %d\n", product);
+    printf("Quotient: %f\n", quotient);
+}

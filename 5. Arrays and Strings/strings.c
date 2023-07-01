16930f3e95dd04c22864191859dc087e78dc1a1e
Abhinav Kayastha <abhinavk@metropolia.fi>
2023-07-01 18:58:00 +0300

    learning strings

---
+++ 5. Arrays and Strings/strings.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    strings syntax:
+
+    declaration:
+    char string_name[];
+
+    initialization:
+    char string_name[] = "String";
+    */
+
+    char name[] = "Abhinav";
+}
